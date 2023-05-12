dd4ef255212db84e8790555ec3bb326f20806398
nkalliney <132940891+nkalliney@users.noreply.github.com>
2023-05-12 14:38:51 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-# Write your code here :-)
 import time
 import board
 import neopixel
@@ -20,13 +19,9 @@ if __name__ == '__main__':
 # Required to control LED colors of NeoPixel
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 1)
 
-# There does not seem to be an inherent ID for each processor. Therefore, assign a unique ID to each processor
-# you are working with. I suggest numbering from 0 - n.
-#########
+# Assign a unique ID to each processor
+# I suggest numbering from 0 - n.
 setup.my_id = 0
-#########
-
-#  while not i2c.try_lock(): pass    #For future use only ... ignore for now ....
 
 #====================================================
 def reply(response):
@@ -36,8 +31,8 @@ def reply(response):
 # This returns data in the format channel + ' ' + data + \n
 def process_command(command):
     com2 = command.decode("utf-8")
-    if com2 == "which\r": # Which channel am I talking to?
-        reply(setup.my_id) # ME!
+    if com2 == "which\r": # if asked, "which channel am I talking to?"
+        reply(setup.my_id) # respond with unique ID
     elif com2 == "info\r":
         reply(info())
     elif com2 == "readmpall\r": # if the command is read all of the channels
@@ -59,11 +54,11 @@ def read_all():
     return data_string
 
 def read_dec(command): #this will receive a command in the format read##\r
-    chan_name = command[-3:-1] #this is the string name of the channel we want to read
+    chan_name = command[-3:-1] #this is the string name of the channel we want to read (##)
     if chan_name not in setup.chan_dict.keys():
         return "Error: This channel ID is not available."
     val = read(setup.chan_dict[chan_name])
-    return str(setup.my_id) + chan_name + ' ' + str(val) + '\n' #val is differential output in V, to find field in Gauss, for this detector, 0.5 G / 1 V
+    return str(setup.my_id) + chan_name + ' ' + str(val) + '\n' #val is differential output in V
 
 def read(chan): # get the value of this channel
     return chan.voltage
@@ -74,14 +69,11 @@ while True:
             # At least one byte in serial input buffer
             byte = serial.read(1)
             if byte == b'\r':
-                # A carriage return (<CR>) character has been received ...
-                out_data = in_data #copy In_Data to Out_Data
-                out_data += b'\r'       #append a <CR> to Out_Data
-                process_command(out_data)
+                # A carriage return character (end of message) has been received ...
+                full_message = in_data #copy In_Data to full_message
+                full_message += b'\r'       #append a carriage return to full_message
+                process_command(full_message)
                 in_data = bytearray()   #Clear In_Data for next command
-                out_index = 0
             else:
                 # Received character is NOT a <CR> so append this character to a growing in_data bytearray.
                 in_data += byte
-                #if len(in_data) == 100:
-                #    raise Exception("Command lengths can not exceed 100!")
